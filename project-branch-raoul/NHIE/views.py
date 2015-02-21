eb2a6a47b01b7fdd9b9cc529293bbe43f85f8d2e
Redfielder <raoul.rothfeld+github@gmail.com>
2015-02-21 16:43:26 +0000

    DELETE old folder

--- project-branch-raoul/NHIE/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Never Have I Ever is online!<br><a href='/NHIE/about'>About</a>")    
-def about(request):
-    return HttpResponse("Never Have I Ever information page!<br><a href='/NHIE/index'>back</a>")
