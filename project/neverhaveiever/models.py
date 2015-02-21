36a797661240b5e30f5d8868a304dc5609ea7bcd
Redfielder <raoul.rothfeld+github@gmail.com>
2015-02-21 18:30:59 +0000

    started work on models

--- project/neverhaveiever/models.py
+++ project/neverhaveiever/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
-
+# Class to store categories
 class Category(models.Model):
-    # statements = models.ManyToManyField(Statement)
     name = models.CharField(max_length=128, unique=True)
-    adult_themed = models.BooleanField(default=False)
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
@@ -15,13 +13,11 @@ class Category(models.Model):
     def __unicode__(self):
         return self.name
 
-
+# Class to store statements
 class Statement(models.Model):
     categories = models.ManyToManyField(Category)
     title = models.CharField(max_length=128)
     views = models.IntegerField(default=0)
-    no_answers = models.IntegerField(default=0)
-    yes_answers = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
@@ -30,3 +26,47 @@ class Statement(models.Model):
 
     def __unicode__(self):
         return self.title
+
+# Class to store play sessions
+class Session(models.Model):
+    statements = models.ManyToManyField(Statement)
+    id = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Session, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.id
+
+# Class to store players
+class Player(models.Model):
+    session = models.ManyToOneField(Session)
+    id = models.IntegerField(default=0)
+    gender = models.CharField(max_length=1)
+    age = models.IntegerField()
+    nationality = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Session, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.id
+
+# Class to store answers
+class Anwser(models.Model):
+    session = models.ManyToOneField(Session)
+    statement = models.ManyToOneField(Statement)
+    id = models.IntegerField(default=0)
+    answer = models.BooleanField()
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Anwser, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.id
--- project/populate_app.py
+++ project/populate_app.py
@@ -10,49 +10,57 @@ from neverhaveiever.models import Category, Statement
 
 
 def populate():
-    law_category = add_category('Law')
+    # add categories
+    cat_nsfw = add_category("NSFW")
+    cat_violence = add_category('Violence')
+    cat_illegal = add_category('Illegal')
+    cat_alcohol = add_category("Alcohol")
+    cat_sexual = add_category("Sexual")
+    cat_activity = add_category("Activities")
 
-    add_statement(categories=[law_category],
-                  title="Run a red light",
-                  views=130,
-                  yes_answers=40,
-                  no_answers=50,
+    # add statements
+    add_statement(categories=[cat_activity],
+                  title="been bungee jumping",
+                  views=10,
                   )
 
-    add_statement(categories=[law_category],
-                  title="Stolen something",
-                  views=70,
-                  yes_answers=15
+    add_statement(categories=[cat_illegal],
+                  title="run a red light",
+                  views=130,
+                  )
+
+    add_statement(categories=[cat_nsfw, cat_illegal],
+                  title="stolen something",
+                  views=70
     )
 
-    add_statement(categories=[law_category],
-                  title="Killed a man",
-                  no_answers=50
+    add_statement(categories=[cat_nsfw, cat_violence],
+                  title="killed a person"
     )
 
-    alcohol_category = add_category("Alcohol")
-
-    add_statement(categories=[alcohol_category],
-                  title="Thrown up after drinking too much",
-                  views=600
+    add_statement(categories=[cat_nsfw, cat_alcohol],
+                  title="thrown up after drinking too much",
+                  views=60
     )
 
-    add_statement(categories=[alcohol_category, law_category],
-                  title="Got drunk when I was 15",
-                  views=600
+    add_statement(categories=[cat_nsfw, cat_alcohol, cat_illegal],
+                  title="got drunk when I was underage"
     )
 
-    sex_category = add_category(name="Sex", adult_themed=True)
+    add_statement(categories=[cat_nsfw, cat_sexual],
+                  title="had a threesome"
 
-    add_statement(categories=[sex_category],
-                  title="Had a threesome",
-                  views=500
     )
 
-    add_statement(categories=[sex_category],
-                  title="Had sex in public",
-                  views=500
+    add_statement(categories=[cat_nsfw, cat_sexual],
+                  title="Had sex in public"
     )
+
+    # add sessions
+    add
+
+    # add players
+    # add answers
 
     # Print out what we have added to the user.
     for s in Statement.objects.all():
@@ -62,18 +70,18 @@ def populate():
         print ")"
 
 
-def add_statement(categories, title, views=0, yes_answers=0, no_answers=0):
-    s = Statement.objects.get_or_create(title=title, views=views,
-                                        yes_answers=yes_answers, no_answers=no_answers)[0]
+def add_category(name, adult_themed=False):
+    c = Category.objects.get_or_create(name=name)[0]
+    return c
+
+def add_statement(categories, title, views=0):
+    s = Statement.objects.get_or_create(title=title, views=views)[0]
     for category in categories:
         s.categories.add(category)
     s.save()
     return s
 
-
-def add_category(name, adult_themed=False):
-    c = Category.objects.get_or_create(name=name, adult_themed=adult_themed)[0]
-    return c
+# add further categories! TODO raoul
 
 # Start execution here!
 if __name__ == '__main__':
