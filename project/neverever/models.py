2e278b70afd64231c4b04b8da4e9a7ac49e88212
Redfielder <raoul.rothfeld+github@gmail.com>
2015-02-23 21:01:37 +0000

    Trying some more

--- project/neverever/models.py
+++ project/neverever/models.py
@@ -30,44 +30,26 @@ class Statement(models.Model):
 # Class to store play sessions
 class Session(models.Model):
     statements = models.ManyToManyField(Statement)
-    id = models.IntegerField(primary_key=True)
-    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.id)
         super(Session, self).save(*args, **kwargs)
-
-    def __unicode__(self):
-        return self.id
 
 # Class to store players
 class Player(models.Model):
     session = models.ManyToManyField(Session)
-    id = models.IntegerField(primary_key=True)
     gender = models.CharField(max_length=1)
     age = models.IntegerField()
     nationality = models.CharField(max_length=128)
-    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.id)
         super(Player, self).save(*args, **kwargs)
-
-    def __unicode__(self):
-        return self.id
 
 # Class to store answers
 class Answer(models.Model):
     session = models.ManyToManyField(Session)
     statement = models.ManyToManyField(Statement)
     player = models.ManyToManyField(Player)
-    id = models.IntegerField(primary_key=True)
     answer = models.BooleanField(default=False)
-    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.id)
         super(Answer, self).save(*args, **kwargs)
-
-    def __unicode__(self):
-        return self.id
