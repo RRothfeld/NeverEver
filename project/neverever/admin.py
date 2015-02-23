be3205e50b5db49eb9b490dd90b4135f0c49cc5f
Redfielder <raoul.rothfeld+github@gmail.com>
2015-02-23 22:08:54 +0000

    Added all models to admin view

--- project/neverever/admin.py
+++ project/neverever/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from neverever.models import Category, Statement
+from neverever.models import Category, Statement, Session, Player, Answer
 
 
 admin.site.register(Category)
 admin.site.register(Statement)
+admin.site.register(Session)
+admin.site.register(Player)
+admin.site.register(Answer)
