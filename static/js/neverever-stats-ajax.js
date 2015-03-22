c2a632c16905bbf307eb5d44394c43f847843e64
Redfielder <raoul.rothfeld+github@gmail.com>
2015-03-22 23:01:58 +0000

    quick fix AJAX

--- static/js/neverever-stats-ajax.js
+++ static/js/neverever-stats-ajax.js
@@ -1,31 +1,32 @@
+$(document).ready(function() {
+        /*$('.menu > li').mouseover(function() {
+            //alert("bla");
+            $(this).find('ul').css('visibility', 'visible');
+        });*/
 
-	/*$('.menu > li').mouseover(function() {
-		//alert("bla");
-		$(this).find('ul').css('visibility', 'visible');
-	});*/
+    $("body").on('mouseover', '#catlist', function() {
+        //alert("bla");
+        $(this).find('ul').css('visibility', 'visible');
+    });
 
-$("body").on('mouseover', '#catlist', function() {
-	//alert("bla");
-	$(this).find('ul').css('visibility', 'visible');
-});
+    $("body").on('mouseout', '.menu > li', function() {
+        $(this).find('ul').css('visibility', 'hidden');
+    });
 
-$("body").on('mouseout', '.menu > li', function() {
-	$(this).find('ul').css('visibility', 'hidden');
-});
+    /*$('.cat_list_item').click(function() {
+        var cat_name
+        cat_name = $(this).html();
+        $.get('/stats_test/', {cat_name: cat_name}, function(data) {
+            $("body").html(data);
+        });
+    });*/
 
-/*$('.cat_list_item').click(function() {
-	var cat_name
-	cat_name = $(this).html();
-	$.get('/stats_test/', {cat_name: cat_name}, function(data) {
-		$("body").html(data);
-	});
-});*/
-
-$("body").on('click', '.cat_list_item', function() {
-	//alert("come on");
-	var cat_name
-	cat_name = $(this).html();
-	$.get('/stats_test/', {cat_name: cat_name}, function(data) {
-		$('#statements').html(data);
-	});
+    $("body").on('click', '.cat_list_item', function() {
+        //alert("come on");
+        var cat_name
+        cat_name = $(this).html();
+        $.get('/stats_test/', {cat_name: cat_name}, function(data) {
+            $('#statements').html(data);
+        });
+    });
 });
