9101a9979350dcb893ccbe5ad401e70d27618d90
tvz <tvzeller@gmail.com>
2015-03-22 22:59:35 +0000

    separated out stats ajax stuff

---
+++ static/js/neverever-stats-ajax.js
@@ -0,0 +1,31 @@
+
+	/*$('.menu > li').mouseover(function() {
+		//alert("bla");
+		$(this).find('ul').css('visibility', 'visible');
+	});*/
+
+$("body").on('mouseover', '#catlist', function() {
+	//alert("bla");
+	$(this).find('ul').css('visibility', 'visible');
+});
+
+$("body").on('mouseout', '.menu > li', function() {
+	$(this).find('ul').css('visibility', 'hidden');
+});
+
+/*$('.cat_list_item').click(function() {
+	var cat_name
+	cat_name = $(this).html();
+	$.get('/stats_test/', {cat_name: cat_name}, function(data) {
+		$("body").html(data);
+	});
+});*/
+
+$("body").on('click', '.cat_list_item', function() {
+	//alert("come on");
+	var cat_name
+	cat_name = $(this).html();
+	$.get('/stats_test/', {cat_name: cat_name}, function(data) {
+		$('#statements').html(data);
+	});
+});
