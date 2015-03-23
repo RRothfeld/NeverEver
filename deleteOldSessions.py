7cd6ee6c8079280cbd4360f5a924293bd14924b5
Luke McPhee <lmcphee89@gmail.com>
2015-03-23 14:07:17 +0000

    script ending sessions will now add results to results model

--- deleteOldSessions.py
+++ deleteOldSessions.py
@@ -6,13 +6,23 @@ import django
 
 django.setup()
 
-from neverever.models import Session
+from neverever.models import Session, Result
 
 from datetime import datetime, timedelta
 
 num_hours = 1
 
-sessions = Session.objects.filter(last_modified__lte=datetime.now()-timedelta(hours=num_hours))
+# sessions = Session.objects.filter(last_modified__lte=datetime.now()-timedelta(hours=num_hours))
+
+# debug line below to just bring in all sessions
+sessions = Session.objects.all()
+print sessions
+
+for session in sessions:
+    questions = session.used_statements.all()
+    for question in questions:
+        statement = question
+        result = Result.objects.create(statement=statement)
 
 # print len(sessions), " old sessions were detected"
 
