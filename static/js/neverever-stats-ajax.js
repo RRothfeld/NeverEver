fcbe4019157b98436f19a240f7cff96e437d9a65
tvz <tvzeller@gmail.com>
2015-03-24 08:29:30 +0000

    and some more

--- static/js/neverever-stats-ajax.js
+++ static/js/neverever-stats-ajax.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    // loads relevant statement titles when a category is selected on stats page
     $("body").on('click', '.cat_list_item', function() {
         var cat_name
         cat_name = $(this).attr('data-name');
@@ -8,6 +9,7 @@ $(document).ready(function() {
         });
     });
 
+    // loads relevant statement stats when a statement is selected on stats page
     $("body").on('click', '.single_statement', function() {
         var statement_title;
         statement_title = $(this).attr('data-title');
