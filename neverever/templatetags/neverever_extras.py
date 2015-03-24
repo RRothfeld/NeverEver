f1f2215421ccb4fb6ae0c0cc9f930818b279039f
tvz <tvzeller@gmail.com>
2015-03-24 00:20:47 +0000

    views now has more comments; could still do with some refactoring...

--- neverever/templatetags/neverever_extras.py
+++ neverever/templatetags/neverever_extras.py
@@ -9,15 +9,11 @@ register = template.Library()
 
 @register.inclusion_tag('neverever/answerButtons.html')
 def get_answer_buttons(session):
-    
     players = Player.objects.filter(session=session)
-
     forms = []
-
     for i in range(0, len(players)):  # +1 since a new player was added
         forms.append(AnswerForm(prefix="form" + str(i)))
 
-    players = Player.objects.filter(session = session)
     formlist = zip(forms, players)
     
     return {'formlist': formlist}
--- neverever/views.py
+++ neverever/views.py
@@ -17,23 +17,13 @@ import random  # Fetch random statements
 
 # view for the home page
 def index(request):
-    context_dict = {}
-    sid = request.session.session_key
-    if not sid:
-        request.session.save()
-        request.session.modified = True
-        sid = request.session.session_key
-    context_dict['sid'] = sid
-    num_sessions = Session.objects.count()
-    context_dict['nSessions'] = num_sessions
-    num_players = Player.objects.count()
-    context_dict['nPlayers'] = num_players
-    return render(request, 'neverever/index.html', context_dict)
+    return render(request, 'neverever/index.html')
 
 # view for updating the global session and player count on home page
 # called by AJAX script 
 def update_count(request):
     context_dict = {}
+    # get info on current number of games and players to pass to template
     num_sessions = Session.objects.count()
     context_dict['nSessions'] = num_sessions
     num_players = Player.objects.count()
@@ -46,14 +36,15 @@ def about(request):
 
 # display overall stats page
 def stats(request):
+    # get info on total games and players
     gc = GlobalCounter.objects.all()[0]
     results = Result.objects.all()
     context_dict = {'globalCounter': gc, 'results': results}
-
+    # get info on number of categories, statements and answers
     context_dict['nCategories'] = len(Category.objects.all())
     context_dict['nStatements'] = len(Statement.objects.all())
     context_dict['nAnswers'] = len(Answer.objects.all()) + len(Result.objects.all())
-
+    # get list of categories to use in template
     categories = Category.objects.all();
     context_dict['categories'] = categories
     return render(request, 'neverever/stats.html', context_dict)
@@ -62,13 +53,12 @@ def stats(request):
 # (called using AJAX)
 def stats_test(request):
     if request.method == 'GET':
+        # get category name received from AJAX get request
         cat_name = request.GET['cat_name']
-        print cat_name
 
     category = Category.objects.get(name=cat_name)
-
+    # get list of statements in that category
     statements = Statement.objects.filter(categories=category)
-
     return render(request, 'neverever/statementTitles.html', {'statements': statements})
 
 # display statistics on an individual statement on overall stats page
@@ -76,42 +66,47 @@ def stats_test(request):
 def statement_info(request):
     statement_title = ""
     if request.method == 'GET':
+        # get statement title received from AJAX get request
         statement_title = request.GET['title']
 
     statement = Statement.objects.get(title=statement_title)
-    print statement
-
+    # set up stats variables
     yes = 0
     no = 0
-    print statement
     female_yes = 0
     male_yes = 0
+    female_percentage = 0
+    male_percentage = 0
     yes_age=0
     no_age=0
     avg_yes_age=0
     avg_no_age=0
-
-    statement_answers = Result.objects.filter(statement=statement)
     yes_nationalities = []
-
+    #get list of answers for this statement
+    statement_answers = Result.objects.filter(statement=statement)
     for result in statement_answers:
+        # if answer is yes
         if result.answer:
             yes += 1
+            # if player provided their age
             if result.age:
                 yes_age += result.age
+            # add player nationality to list of nationalities
             yes_nationalities.append(result.nationality)
-            if(result.gender == 'f'):
+            # increment female and male yes counts
+            if(result.gender == 'F'):
                 female_yes += 1
-            elif(result.gender == 'm'):
+            elif(result.gender == 'M'):
                 male_yes += 1
+        # if answer is no
         else:
             no += 1
+            # if player provided their age
             if result.age:
                 no_age += result.age
 
     total = yes+no
-    female_percentage = 0
-    male_percentage = 0
+    # if this statement has answers, calculate yes and no percentages
     if total > 0:
         yes_percentage = (yes*100/total)
         no_percentage = (no*100/total)
@@ -119,6 +114,7 @@ def statement_info(request):
         yes_percentage = False
         no_percentage = False
     
+    # if this statement has yes answers, calculate female and male percentages for yes answers
     if yes > 0:
         female_percentage = (female_yes*100/yes)
         male_percentage = (male_yes*100/yes)
@@ -126,8 +122,10 @@ def statement_info(request):
     if no > 0:
         avg_no_age = no_age/no
 
+    # get frequencies for nationalities who said yes
     nat_freqs = Counter(yes_nationalities).most_common()
 
+    # set up context_dict to pass to template
     context_dict = {}
     context_dict['title'] = statement
     context_dict['yes'] = yes
@@ -162,13 +160,14 @@ def play(request):
         context_dict['session'] = "Already existed"
 
     else:
+        # if session is new, set up session
         context_dict['session'] = "Just created"
         session.nsfw = SESSION_NSFW
         for cat in Category.objects.all():
             session.categories.add(cat)
 
         session.save()
-        # manually creating player 1
+        # manually create player 1
         p1 = Player.objects.create(stamp=1, session=session)
 
         # Update global counters
@@ -179,37 +178,37 @@ def play(request):
 
     print "SESSION last_modified:", session.last_modified
     categories = session.categories.all()
-    context_dict['categories'] = categories
+    #context_dict['categories'] = categories
 
     # Testing players
     players = Player.objects.filter(session=session)
-    context_dict['Players'] = players
+    #context_dict['Players'] = players
 
+    # if we get answers back from the client
     if request.method == 'POST':
-
-        # Testing
         session = Session.objects.filter(sid=sid)[0]
+        # get statement that has just been answered
         used_statement = session.last_statement
         session.used_statements.add(used_statement)
 
         session = Session.objects.get(sid=sid)
+        # get number of players in this game
         num_players = len(Player.objects.filter(session=session))
         forms = []
+        # get answer forms for each player
         for i in range(0, num_players):
             forms.append(AnswerForm(request.POST, prefix="form" + str(i)))
 
+        #loop through answer forms, give answers remaining information and save
         for i in range(0, num_players):
             if forms[i].is_valid():
                 answer = forms[i].save(commit=False)
                 answer.statement = used_statement
                 answer.session = Session.objects.get(sid=sid)
-                answer.player = players[i]  # Player.objects.get(stamp=123)
+                answer.player = players[i] 
                 answer.save()
             else:
                 print forms[i].errors
-
-
-    #num_players = len(Player.objects.filter(session=session))
 
     # Pick a random statement from the selected categories
     while True:
@@ -238,14 +237,12 @@ def play(request):
 
     context_dict['statement'] = rand_statement
 
-    # Testing
+    # save statement to be used in this round as the last statement
     session.last_statement = rand_statement
     session.save()
 
+    # put session in context_dict to pass to template
     context_dict['this_session'] = session
-
-    num_players = len(Player.objects.filter(session=session))
-    context_dict["nPlayers"] = num_players
     
     response = render(request, 'neverever/play.html', context_dict)
     return response
@@ -254,12 +251,15 @@ def play(request):
 def like_statement(request):
     title = None
     if request.method == 'GET':
+        # get statement title received from AJAX get request
         title = request.GET['title']
 
     likes = 0
     if title:
+        # get statement object from title
         statement = Statement.objects.get(title=title)
         if statement:
+            # increment likes and save
             likes = statement.likes+1
             statement.likes = likes
             statement.save()
@@ -269,26 +269,39 @@ def like_statement(request):
 # view to save names entered by the players on the gameplay page
 # and display the updated page using AJAX
 def set_name(request):
-    name = request.GET['name']
-    num = request.GET['stamp']
+    name = None
+    title = None
+    if request.method == 'GET':
+        # get player's new name and player number from AJAX get request
+        name = request.GET['name']
+        num = request.GET['stamp']
+    
     sid = request.session.session_key
     session = Session.objects.get(sid=sid)
-    #pass it both stamp and session to make sure its the right player
-    player = Player.objects.get(stamp=num, session=session)
-    player.name = name
-    player.save()
+    
+    if name:
+        #pass it both stamp and session to make sure its the right player and save their name
+        player = Player.objects.get(stamp=num, session=session)
+        player.name = name
+        player.save()
 
+    # get num players and put an answer form for each player in list
     num_players = len(Player.objects.filter(session=session))
     forms = []
     for i in range(0, num_players):
         forms.append(AnswerForm(prefix="form" + str(i)))
 
+    # get list of players in this game
     players = Player.objects.filter(session = session)
+    # zip the list of forms and the list of players so we can loop through both in the template
     formlist = zip(forms, players)
 
     return render(request, 'neverever/answerButtons.html', {'formlist': formlist})
 
 # display the end of game summary page
+# we get information on the players then store this info
+# along with info on the respective answers in a results model
+# the rest of the data is deleted (session, players, answers) after the game is finished
 def play_summary(request):
     context_dict = {}
     sid = request.session.session_key
@@ -300,64 +313,59 @@ def play_summary(request):
         request.session.save()
         request.session.modified = True
         sid = request.session.session_key
+    
     context_dict['sid'] = sid
-
+    
+    # if we are getting the player forms back from the client
     if request.method == 'POST':
-        # this is where we put things if the form has been submitted
         session = Session.objects.get(sid=sid)
         num_players = len(Player.objects.filter(session=session))
         forms = []
-
+        # loop through forms and put in list
         for i in range(0, num_players):
-            #count = 1
             p = Player.objects.get(stamp=i+1, session=session)  # TODO change so different player
             forms.append(PlayerForm(request.POST, prefix="form" + str(i), instance=p))
-            #count = count+1
 
+        # loop through player forms and save each player object
         for i in range(0, num_players):
             if forms[i].is_valid():
                 player = forms[i].save(commit=False)
-                # note need to make sure is saving their details here
-                # player.stamp =
-                player.session = Session.objects.get(sid=sid)
+                player.session = session
                 player.save()
             else:
                 print forms[i].errors
 
-
-        # update the response model
-        responses = Answer.objects.all()
+        # get answers belonging to this session
+        responses = Answer.objects.filter(session=session)
+        # go through each answer and extract relevant information, then make new result object with that info
         for answer in responses:
-            s = Session.objects.get(sid=sid)
-            if answer.session == s:
-                statement = answer.statement
-                ans = answer.answer
-                gender = answer.player.gender
-                nationality = answer.player.nationality
-                age = answer.player.age
-                #use create not get_or_create as results don't have to be unique
-                result = Result.objects.create(statement=statement, answer=ans, gender=gender,
-                                                      nationality=nationality, age=age)
+            statement = answer.statement
+            ans = answer.answer
+            gender = answer.player.gender
+            nationality = answer.player.nationality
+            age = answer.player.age
+            #use create not get_or_create as results don't have to be unique
+            result = Result.objects.create(statement=statement, answer=ans, gender=gender,
+                                                  nationality=nationality, age=age)
 
-        # end session
+        # delete session
         try:
-            s = Session.objects.get(sid=sid)
-            s.delete()
+            session.delete()
             return HttpResponseRedirect('/')
-            #response = HttpResponse("Session has ended")
         except:
             response = HttpResponse("Something went wrong. Probably ending the session")
         return response
 
+    # else if request is GET
     else:
-        #display the forms for each user
+        #display the forms for each player
         session = Session.objects.get(sid=sid)
         num_players = len(Player.objects.filter(session=session))
         forms = []
         player_names = []
         for i in range(0, num_players):
             forms.append(PlayerForm(prefix="form" + str(i)))
-            player = Player.objects.get(stamp=i+1)
+            player = Player.objects.get(stamp=i+1, session=session)
             if player.name:
                 player_names.append(player.name)
             else:
@@ -365,10 +373,10 @@ def play_summary(request):
 
     context_dict['player_forms'] = zip(player_names, forms)
 
-    players = Player.objects.filter(session=session).order_by('id')
-
+    players = Player.objects.filter(session=session)
     player_answers = []
     player_total_yes_no = []
+    # get answers stats for each player
     for player in players:
         player_answers.append(Answer.objects.filter(player=player))
 
@@ -380,12 +388,13 @@ def play_summary(request):
                                     'total': total_yes + total_no})
 
     player_total_yes_no = sorted(player_total_yes_no, key=itemgetter('total_yes'), reverse=True)
+    # put stats in  context_dict to pass to template
     context_dict['player_answers'] = player_answers
     context_dict['player_total_yes_no'] = player_total_yes_no
 
     statements = session.used_statements.all()
-
     statement_data = []
+    # get answer stats for each statement
     for statement in statements:
         answers = Answer.objects.filter(session=session, statement=statement).order_by('player__stamp')
         count_yes = answers.filter(answer=True).count()
@@ -398,8 +407,7 @@ def play_summary(request):
                 'answers': answers}
         statement_data.append((statement, data))
 
-
-
+    # put stats in context_dict to pass to template
     context_dict['statement_data'] = statement_data
     response = render(request, 'neverever/playSummary.html', context_dict)
     return response
@@ -415,12 +423,14 @@ def play_options(request):
         return index(request)
 
     if request.method == 'POST':
+        # save session form obtained from options page with session categories
         form = SessionForm(request.POST, instance=session)
         if form.is_valid():
             s = form.save(commit=True)
             return HttpResponseRedirect('/play')
         else:
             print form.errors
+    # else, if GET request
     else:
         form = SessionForm(instance=session)
 
@@ -432,14 +442,10 @@ def new_statement(request):
     context_dict = {}
 
     if request.method=='POST':
+        # save statement form obtained from new statement page
         form = StatementForm(request.POST)
         if form.is_valid():
             statement = form.save(commit=False)
-            # if 'categories' in request.POST.keys():
-            #    for
-            # statement.save()
-            # return HttpResponseRedirect(request)
-            s = ""
             statement.save()
             for k in form.cleaned_data['categories']:
                 statement.categories.add(Category.objects.filter(id=k)[0])
@@ -447,6 +453,7 @@ def new_statement(request):
             return HttpResponseRedirect('/')
         else:
             print form.errors
+    # else, if GET request
     else:
         form = StatementForm()
 
@@ -466,15 +473,17 @@ def add_player(request):
         session.save()
         #create more players
         print "NEW PLAYER:", (len(players) + 1)
+        # create new player in this session
         Player.objects.create(stamp=(len(players) + 1), session = session)
 
     forms = []
-    session = Session.objects.get(sid=(sid))
-
+    # add a form for each player to list
     for i in range(0, len(players)+1):  # +1 since a new player was added
         forms.append(AnswerForm(prefix="form" + str(i)))
 
+    # get list of players in this session
     players = Player.objects.filter(session = session)
+    # zip list of forms and players so that we can loop through both in the template
     formlist = zip(forms, players)
 
     rendered = str(render_to_string('neverever/answerButtons.html',
